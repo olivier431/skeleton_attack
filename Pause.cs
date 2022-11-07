437ba889badfc53e38ea52c7c29ad5af05409c9f
Olivier Lefebvre <oli_43@hotmail.fr>
2022-11-07 16:49:08 -0500

    Pause done

---
+++ Pause.cs
@@ -0,0 +1,67 @@
+using Godot;
+using System;
+
+public class Pause : CanvasLayer
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	TextureRect black;
+	Button button;
+	Button button2;
+	Button button3;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		black = GetNode<TextureRect>("TextureRect");
+		button = GetNode<Button>("VBoxContainer/Button");
+		button2 = GetNode<Button>("VBoxContainer/Button2");
+		button3 = GetNode<Button>("VBoxContainer/Button3");
+	}
+
+	private void _on_Button_pressed()
+	{
+		black.Visible = !black.Visible;
+		button.Visible = !button.Visible;
+		button2.Visible = !button2.Visible;
+		button3.Visible = !button3.Visible;
+		GetTree().Paused = !GetTree().Paused;
+	}
+	
+	private void _on_Button2_pressed()
+	{
+		GetTree().Paused = !GetTree().Paused;
+		GetTree().ChangeScene("res://Menu.tscn");
+	}
+	
+	private void _on_Button3_pressed()
+	{
+		GetTree().Quit();
+	}
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+	
+  }
+
+	public override void _Input(InputEvent inputEvent)
+	{
+		if (Input.IsActionPressed("Pause"))
+		{
+			black.Visible = !black.Visible;
+			button.Visible = !button.Visible;
+			button2.Visible = !button2.Visible;
+			button3.Visible = !button3.Visible;
+			GetTree().Paused = !GetTree().Paused;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
